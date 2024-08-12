b452644953d99aaba27e0efbaa962d534627d928
chenlaug <ch.laughan@gmail.com>
2024-08-12 23:31:56 +0200

    chore: Update lang attribute in HTML tag to "fr"

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" suppressHydrationWarning>
+    <html lang="fr">
       <body className={inter.className}>
         <ReactQueryProvider>
           <ThemeProvider
